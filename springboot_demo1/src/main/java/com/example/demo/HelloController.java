772c61355c0871b5906246f5c67fbaa363f24a0b
chapy0706 <chapy0706@outlook.com>
2025-07-30 22:50:13 +0900

    4.リクエストパラメータを処理(2)

--- springboot_demo1/src/main/java/com/example/demo/HelloController.java
+++ springboot_demo1/src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,14 @@ public class HelloController {
  @RequestMapping("/hello")
  public String show() {
   return "Hello!!";
+ }
+ 
+ @RequestMapping(value = "/hello", method = RequestMethod.POST)
+ public String show(@RequestParam("nameParameter") String name, @RequestParam("numOfTimes") int numberOfTimes) {
+  StringBuilder sb = new StringBuilder();
+  for (int i = 0; i < numberOfTimes; i++) {
+   sb.append(name + "さんこんにちは!<br>");
+  }
+  return sb.toString();
  }
 }
