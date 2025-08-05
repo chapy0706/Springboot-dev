9c941db5489682e3045fa108a2745ffb1aa917e4
chapy0706 <chapy0706@outlook.com>
2025-08-06 00:27:41 +0900

    7.Web開発①　Step1:おみくじAPIの作成

---
+++ springboot_demo1/src/main/java/com/example/demo/omikuji.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class omikuji {
+
+    private final List<String> fortunes = List.of(
+        "大吉", "吉", "吉", "吉", "小吉", "凶"
+    );
+
+    private final Random random = new Random();
+
+    @GetMapping("/omikuji")
+    public String omikuji() {
+        int index = random.nextInt(fortunes.size());
+        return fortunes.get(index);
+    }
+}
