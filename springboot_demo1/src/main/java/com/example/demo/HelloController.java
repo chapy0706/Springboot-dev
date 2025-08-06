127367a2a944c2a1162bdac439aac725cbad0a69
chapy0706 <chapy0706@outlook.com>
2025-08-06 11:50:15 +0900

    7.Web開発①　Step2:10年後の年齢計算API(クエリストリング)

--- springboot_demo1/src/main/java/com/example/demo/HelloController.java
+++ springboot_demo1/src/main/java/com/example/demo/HelloController.java
@@ -1,23 +1,29 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
- @RequestMapping("/hello")
- public String show() {
-  return "Hello!!";
- }
- 
- @RequestMapping(value = "/hello", method = RequestMethod.POST)
- public String show(@RequestParam("nameParameter") String name, @RequestParam("numOfTimes") int numberOfTimes) {
-  StringBuilder sb = new StringBuilder();
-  for (int i = 0; i < numberOfTimes; i++) {
-   sb.append(name + "さんこんにちは!<br>");
-  }
-  return sb.toString();
- }
+// @RequestMapping("/hello")
+// public String show() {
+//  return "Hello!!";
+// }
+// 
+// @RequestMapping(value = "/hello", method = RequestMethod.POST)
+// public String show(@RequestParam("nameParameter") String name, @RequestParam("numOfTimes") int numberOfTimes) {
+//  StringBuilder sb = new StringBuilder();
+//  for (int i = 0; i < numberOfTimes; i++) {
+//   sb.append(name + "さんこんにちは!<br>");
+//  }
+//  return sb.toString();
+// }
+
+   @RequestMapping("/hello")
+   public String helloQueryString(@RequestParam String name, @RequestParam int age) {
+	 age = age + 10; // ageを10年後として設定する
+	 return "こんにちは" + name + "さん。10年後は" + age + "歳ですね!";
+   }
 }
