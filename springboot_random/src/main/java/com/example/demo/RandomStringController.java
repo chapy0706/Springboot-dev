fb20cc111e24fa37317ede5b90ada3e0e96b1b60
chapy0706 <chapy0706@outlook.com>
2025-08-18 04:58:52 +0900

    16.Spring MVC➁

---
+++ springboot_random/src/main/java/com/example/demo/RandomStringController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RandomStringController {
+
+ @Autowired
+ RandomStringGenerator gen;
+
+ @RequestMapping("/")
+ public String index() {
+  return "random";
+ }
+
+ @RequestMapping(value = "/generate", method = RequestMethod.POST)
+public ModelAndView generate(@RequestParam("charLength") int charLength,
+@RequestParam(name = "withNumber", defaultValue = "") String withNumber, ModelAndView mv) {
+  boolean withNumFlag = "1".equals(withNumber);
+  List<String> randomStringList = gen.generate(charLength, withNumFlag);
+  mv.addObject("randList", randomStringList);
+
+  mv.setViewName("random");
+  return mv;
+ }
+}
